bae93b3625d059c9685103008f3fd3a414f2ca31
sergiopoliveira <sergiopoliveira@outlook.com>
2018-06-08 15:55:36 +0100

    formatting

--- src/main/java/sergio/config/WebConfig.java
+++ src/main/java/sergio/config/WebConfig.java
@@ -1,13 +1,13 @@
 package sergio.config;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
-
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
 import sergio.domain.Recipe;
 import sergio.services.RecipeService;
